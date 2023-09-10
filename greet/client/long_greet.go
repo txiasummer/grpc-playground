4ac280f49963cfb5cb314c64ea21db77d15f2068
Tracy Xia <tracyxia@carfax.com>
2023-09-10 17:24:35 -0400

    Implemented greet gRPC service with client side streaming

---
+++ greet/client/long_greet.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+	pb "grpc-playground/greet/proto"
+	"log"
+	"time"
+)
+
+func doLongGreet(c pb.GreetServiceClient) {
+	log.Println("doLongGreet was invoked")
+	reqs := []*pb.GreetRequest{
+		{FirstName: "Hailey"},
+		{FirstName: "Martel"},
+		{FirstName: "Dandelion"},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("Error while calling LongGreet: %v\n", err)
+	}
+	for _, req := range reqs {
+		log.Printf("Sending req: %v\n", req)
+		stream.Send(req)
+		time.Sleep(1 * time.Second)
+	}
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error while receiving response from LongGreet: %v\n", err)
+	}
+	log.Printf("LongGreet: %s\n", res.Result)
+}
---
+++ greet/server/long_greet.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	pb "grpc-playground/greet/proto"
+	"io"
+	"log"
+)
+
+func (s *Server) LongGreet(stream pb.GreetService_LongGreetServer) error {
+	log.Println("LongGreet function was invoked")
+	res := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.GreetResponse{Result: res})
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v\n", err)
+		}
+		log.Printf("Receiving: %v\n", req)
+		res += fmt.Sprintf("Hello %s!\n", req.FirstName)
+	}
+}
