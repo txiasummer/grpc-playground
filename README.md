e2aa35f1eb97b5d8040c59dcbc7134fc4bf59ce5
Tracy Xia <tracyxia@carfax.com>
2023-09-10 13:27:43 -0400

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+## gRPC-playground
+
+This is a little tutorial project to demonstrate setting up a simple gRPC client and server pair. 
---
+++ greet/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "grpc-playground/greet/proto"
+)
+
+const addr = "0.0.0.0:50051"
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+func main() {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("Failed to listen on %v\n", err)
+	}
+	log.Printf("Listening on %s\n", addr)
+	s := grpc.NewServer()
+	if err = s.Serve(listener); err != nil {
+		log.Fatalf("Failed to serve: %v\n", err)
+	}
+}
