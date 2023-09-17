bd3349fcbdd517ca8c3b3a5c9224088f49c5e27f
Tracy Xia <tracyxia@carfax.com>
2023-09-17 12:27:10 -0400

    Implemented average gRPC service

--- calculator/client/average.go
+++ calculator/client/average.go
@@ -12,14 +12,14 @@ func doAverage(c pb.CalculatorServiceClient) {
 	if err != nil {
 		log.Fatalf("Error while calling doAverage: %v\n", err)
 	}
-	for _, req := range []int32{123, 234, 345, 456, 567, 678, 789} {
-		log.Printf("Sending req: %v\n", req)
-		stream.Send(&pb.AverageRequest{X: req})
+	for _, num := range []int32{123, 234, 345, 456, 567, 678, 789} {
+		log.Printf("Sending number: %v\n", num)
+		stream.Send(&pb.AverageRequest{X: num})
 	}
 
 	res, err := stream.CloseAndRecv()
 	if err != nil {
 		log.Fatalf("Error while receiving response from doAverage: %v\n", err)
 	}
-	log.Printf("doAverage result: %s\n", res.Result)
+	log.Printf("doAverage result: %f\n", res.Result)
 }
